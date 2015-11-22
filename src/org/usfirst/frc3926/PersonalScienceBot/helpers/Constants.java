7b922fe636bfb8bfcec9f271ee0c6841bac473f1
Bryan <bryan@Atomic-Diamonds-MacBook-Pro.local>
2015-11-21 20:41:40 -0800

    Init Commit
    initial commit

---
+++ src/org/usfirst/frc3926/PersonalScienceBot/helpers/Constants.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc3926.PersonalScienceBot.helpers;
+
+public final class Constants {
+	
+	private Constants() {}
+	
+	public static final double
+	JOYSTICK_DEADZONE = 0.1,
+	ENCODER_DEADZONE = 0.2;
+
+}
